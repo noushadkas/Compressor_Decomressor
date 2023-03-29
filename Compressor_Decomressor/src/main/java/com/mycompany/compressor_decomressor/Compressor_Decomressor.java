3342271edeabd101f27a586005fd061e511677a7
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-03-30 03:33:37 +0530

    init

---
+++ Compressor_Decomressor/src/main/java/com/mycompany/compressor_decomressor/Compressor_Decomressor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compressor_decomressor;
+
+/**
+ *
+ * @author MOHAMMAD NOUSHAD
+ */
+public class Compressor_Decomressor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
